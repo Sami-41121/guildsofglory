1f830b61ef4f8e031872f697131ac30d27460926
Mohammed Sami Khan <75940047+Sami-41121@users.noreply.github.com>
2020-12-30 13:06:47 +0600

    Update Leaderboard.cpp
    changed to reusable window format

--- Leaderboard.cpp
+++ Leaderboard.cpp
@@ -8,13 +8,8 @@ void Leaderboard::initVariables()
 	* -initializes window
 	* -set strings of buttons
 	*/
-	this->window = nullptr;
 	this->buttonPos = std::vector<sf::Vector2f>(4);
-	this->options = std::vector<std::string>(4);
-	options[0] = "Easy";
-	options[1] = "Medium";
-	options[2] = "Hard";
-	options[3] = "Back to Menu";
+	this->buttonTexture = std::vector<sf::Texture>(4);
 }
 
 void Leaderboard::initFont(sf::Font font)
@@ -41,15 +36,9 @@ void Leaderboard::initText()
 	*/
 	this->fileText = sf::Text("", this->font, 15);
 	this->fileText.setFillColor(sf::Color::White);
-
-	this->buttonText = sf::Text("NONE", this->font, 25);
-	this->buttonText.setFillColor(sf::Color::White);
-
-	this->titleText = sf::Text("Leaderboard", this->font, 30);
-	this->titleText.setFillColor(sf::Color::Yellow);
 }
 
-void Leaderboard::initWindow()
+void Leaderboard::initWindow(sf::RenderWindow *window)
 {
 	/**
 	* @return void
@@ -58,11 +47,35 @@ void Leaderboard::initWindow()
 	* -set window size
 	* -set window frame rate
 	*/
-	this->window = new sf::RenderWindow(sf::VideoMode(900.0f, 600.0f), "Guilds of Glory", sf::Style::Close | sf::Style::Titlebar);
-	this->window->setFramerateLimit(60);
+	this->lbOpen = true;
+	this->window = window;
 }
 
-Leaderboard::Leaderboard(sf::Font font)
+void Leaderboard::initTexture()
+{
+	/**
+	* @return void
+	* load textures from file
+	*/
+	if (!this->background.loadFromFile("Resources/leaderboard_bg.jpg")) {
+		std::cout << "Error loading bgTexture";
+	}
+	if (!this->buttonTexture[0].loadFromFile("Resources/button_easy.png")) {
+		std::cout << "Error loading bgTexture";
+	}
+	if (!this->buttonTexture[1].loadFromFile("Resources/button_medium.png")) {
+		std::cout << "Error loading bgTexture";
+	}
+	if (!this->buttonTexture[2].loadFromFile("Resources/button_hard.png")) {
+		std::cout << "Error loading bgTexture";
+	}
+	if (!this->buttonTexture[3].loadFromFile("Resources/button_back-to-main-menu.png")) {
+		std::cout << "Error loading bgTexture";
+	}
+
+}
+
+Leaderboard::Leaderboard(sf::RenderWindow *window, sf::Font font)
 {
 	/**
 	* This function is the container of the main loop
@@ -71,9 +84,10 @@ Leaderboard::Leaderboard(sf::Font font)
 	*/
 
 	this->initVariables();
-	this->initWindow();
+	this->initWindow(window);
 	this->initFont(font);
 	this->initText();
+	this->initTexture();
 
 }
 
@@ -82,7 +96,7 @@ Leaderboard::~Leaderboard()
 	/**
 	* Destructor for Leaderboard
 	*/
-	delete this->window;
+
 }
 
 const bool Leaderboard::isRunning() const
@@ -93,7 +107,7 @@ const bool Leaderboard::isRunning() const
 	* -true if window is open
 	* -false if window closed
 	*/
-	return this->window->isOpen();
+	return this->lbOpen;
 }
 
 void Leaderboard::setButtonPos()
@@ -104,7 +118,7 @@ void Leaderboard::setButtonPos()
 	*/
 	for (int i = 0; i < 4; i++) {
 		this->buttonPos[i].x = window->getSize().x / 2;
-		this->buttonPos[i].y = window->getSize().y * (0.3 + i * (0.7 / 4));
+		this->buttonPos[i].y = window->getSize().y * (0.3 + i * (0.6 / 4));
 	}
 }
 
@@ -127,18 +141,15 @@ void Leaderboard::pollEvents()
 				switch (clickedButton) {
 				case 0:
 					openEasyLeaderboard();
-					std::cout << "Clicked Button 0\n";
 					break;
 				case 1:
 					openMediumLeaderboard();
-					std::cout << "Clicked Button 1\n";
 					break;
 				case 2:
 					openHardLeaderboard();
-					std::cout << "Clicked Button 2\n";
 					break;
 				case 3:
-					this->window->close();
+					lbOpen = false;
 					break;
 				default:
 					break;
@@ -191,55 +202,29 @@ void Leaderboard::update()
 	this->pollEvents();
 }
 
-void Leaderboard::renderTitle()
-{
-	/**
-	* @return void
-	* -sets the title of the screen
-	*/
-	titleText.setOrigin(sf::Vector2f(this->titleText.getString().getSize() * 12.5f, 0.0f));
-	titleText.setPosition(sf::Vector2f(this->window->getSize().x / 2, this->window->getSize().y * 0.1));	
-	this->window->draw(titleText);
-}
-
 void Leaderboard::renderButtons()
 {
 	/**
 	* @return void
 	*
-	* -render buttons
-	* -set outline and color
+	* -set texture
 	* -draw the buttons
 	* -
 	*/
-	sf::RectangleShape button(sf::Vector2f(500.f, 60.0f));
-	button.setOrigin(250.f, 0.0f);
-	button.setFillColor(sf::Color::Red);
-	button.setOutlineColor(sf::Color(255, 255, 255, 255));
-	button.setOutlineThickness(10.0f);
-	for (auto v : buttonPos) {
-		button.setPosition(v);
+	sf::RectangleShape button(sf::Vector2f(400.f, 60.0f));
+	button.setOrigin(200.f, 0.0f);
+	for (int i = 0; i < 4; i++) {
+		button.setPosition(buttonPos[i]);
+		button.setTexture(&buttonTexture[i]);
 		this->window->draw(button);
 	}
 }
 
-void Leaderboard::renderButtonText()
+void Leaderboard::renderBackground()
 {
-	/**
-	* @return void
-	*
-	* -Renders button text
-	* -Modifies the string of each button
-	* -Modifies origin according to string size
-	* -Draw button text
-	*/
-
-	for (int i = 0; i < 4; i++) {
-		buttonText.setString(this->options[i]);
-		buttonText.setOrigin(sf::Vector2f(this->options[i].size() * 9.f, 0.0f));
-		buttonText.setPosition(this->buttonPos[i]);
-		this->window->draw(this->buttonText);
-	}
+	sf::RectangleShape bg(sf::Vector2f(1280.f, 720.f));
+	bg.setTexture(&background);
+	this->window->draw(bg);
 }
 
 void Leaderboard::render()
@@ -253,9 +238,8 @@ void Leaderboard::render()
 	*/
 	this->window->clear(sf::Color(0, 0, 0, 255));
 
-	this->renderTitle();
+	this->renderBackground();
 	this->renderButtons();
-	this->renderButtonText();
 	this->window->display();
 }
 
@@ -344,23 +328,22 @@ void Leaderboard::renderLeaderboard(std::vector<sf::Text> textList)
 	* -poll event for new window
 	* -print text from vector
 	*/
-	sf::RenderWindow lb(sf::VideoMode(500, 600), "Leaderboard", sf::Style::Close | sf::Style::Titlebar);
-	lb.setFramerateLimit(60);
+	sf::RectangleShape lb(sf::Vector2f(500.f, 600.f));
+	lb.setOrigin(250.f, 300.f);
+	lb.setPosition(this->window->getSize().x * 0.5, this->window->getSize().y * 0.5);
+	bool lbOpen = true;
+	
 
 	int st = 0, ed = std::min(40, (int)textList.size());     // starting and ending index for line printing
 
-	while (lb.isOpen()) {
-
-		sf::Event lbEvent;
-		while (lb.pollEvent(lbEvent)) {
-			switch (lbEvent.type) {
-			case sf::Event::Closed:
-				lb.close();
-				break;
+	while (lbOpen) {
+		while (this->window->pollEvent(this->evnt)) {
+			switch (this->evnt.type) {
+			
 			case sf::Event::KeyPressed:
-				switch (lbEvent.key.code) {
+				switch (this->evnt.key.code) {
 				case sf::Keyboard::Escape:
-					lb.close();
+					lbOpen = false;
 					break;
 
 				case sf::Keyboard::Down:
@@ -379,11 +362,11 @@ void Leaderboard::renderLeaderboard(std::vector<sf::Text> textList)
 				break;
 
 			case sf::Event::MouseWheelMoved:
-				if (lbEvent.mouseWheel.delta < 0 and ed < textList.size()) {
+				if (evnt.mouseWheel.delta < 0 and ed < textList.size()) {
 					st++;
 					ed++;
 				}
-				else if (lbEvent.mouseWheel.delta > 0 and st > 0) {
+				else if (evnt.mouseWheel.delta > 0 and st > 0) {
 					st--;
 					ed--;
 				}
@@ -391,14 +374,14 @@ void Leaderboard::renderLeaderboard(std::vector<sf::Text> textList)
 			}
 		}
 
-		lb.clear();
+		this->window->clear();
 
 		for (int i = st; i < ed; i++) {
-			textList[i].setPosition(sf::Vector2f(0.0f, (i - st) * 15.0));
-			lb.draw(textList[i]);
+			textList[i].setPosition(this->window->getSize().x * 0.5 - 250, this->window->getSize().y * 0.5 - 300 + (i - st) * 15.0);
+			this->window->draw(textList[i]);
 		}
-		lb.display();
+		this->window->display();
 	}
-	
+	this->window->clear();
 
 }
